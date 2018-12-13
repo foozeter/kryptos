faf36e42862c7df72df55761ce7de9679606aa9b
hayashibambi <hayashibambi@gmail.com>
2018-12-14 02:20:50 +0900

    create arrow view

--- app/src/main/java/com/hayashibambi/kryptos/HomeActivity.kt
+++ app/src/main/java/com/hayashibambi/kryptos/HomeActivity.kt
@@ -1,9 +1,11 @@
 package com.hayashibambi.kryptos
 
+import android.animation.ObjectAnimator
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.bottomsheet.BottomSheetBehavior
-import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : AppCompatActivity() {
 
@@ -11,14 +13,30 @@ class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
-        buildSheetBehavior()
+//        setContentView(R.layout.activity_home)
+//        buildSheetBehavior()
+
+        setContentView(R.layout.arrow_test)
+//        val arrow = UpDownArrowDrawable(this)
+//        arrow.isUpToDown = false
+        findViewById<View>(R.id.test_view).apply {
+            setOnClickListener { view ->
+                Toast.makeText(this@HomeActivity, "start the animation!", Toast.LENGTH_SHORT).show()
+                val anim = ObjectAnimator.ofFloat(0f, 1f)
+                anim.duration = 5000
+                anim.addUpdateListener {
+                    (view as UpDownArrow).setProgress(it.animatedFraction)
+//                    ViewCompat.postInvalidateOnAnimation(view)
+                }
+                anim.start()
+            }
+        }
     }
 
     private fun buildSheetBehavior() {
-        sheetBehavior = BottomSheetBehavior.from(fragment_container)
-        root_layout.post {
-            sheetBehavior?.peekHeight = fragment_container.height - menu.height
-        }
+//        sheetBehavior = BottomSheetBehavior.from(fragment_container)
+//        root_layout.post {
+//            sheetBehavior?.peekHeight = fragment_container.height - menu.height
+//        }
     }
 }
---
+++ app/src/main/java/com/hayashibambi/kryptos/UpDownArrow.kt
@@ -0,0 +1,93 @@
+package com.hayashibambi.kryptos
+
+import android.content.Context
+import android.graphics.drawable.Drawable
+import android.graphics.drawable.LayerDrawable
+import android.util.AttributeSet
+import android.view.View
+
+class UpDownArrow(context: Context, attrs: AttributeSet): View(context, attrs) {
+
+    private val arrow = UpDownArrowDrawable(context)
+    private val fitToViewSize: Boolean
+
+    init {
+        val a = context.theme.obtainStyledAttributes(
+            attrs, R.styleable.UpDownArrow, 0, 0
+        )
+
+        arrow.arrowWidth = a.getDimensionPixelSize(
+            R.styleable.UpDownArrow_upa_arrowWidth,
+            arrow.arrowWidth
+        )
+
+        arrow.arrowHeight = a.getDimensionPixelSize(
+            R.styleable.UpDownArrow_upa_arrowHeight,
+            arrow.arrowHeight
+        )
+
+        arrow.arrowThickness = a.getDimensionPixelSize(
+            R.styleable.UpDownArrow_upa_arrowThickness,
+            arrow.arrowThickness.toInt()).toFloat()
+
+        arrow.breakStart = a.getFloat(
+            R.styleable.UpDownArrow_upa_breakStart,
+            arrow.breakStart.toFloat()).toDouble()
+
+        arrow.breakEnd = a.getFloat(
+            R.styleable.UpDownArrow_upa_breakEnd,
+            arrow.breakEnd.toFloat()).toDouble()
+
+        arrow.progress = a.getFloat(
+            R.styleable.UpDownArrow_upa_progress,
+            arrow.progress)
+
+        arrow.isUpToDown = a.getBoolean(
+            R.styleable.UpDownArrow_upa_isUpToDown,
+            arrow.isUpToDown)
+
+        arrow.skipBreakTime = a.getBoolean(
+            R.styleable.UpDownArrow_upa_skipBreakTime,
+            arrow.skipBreakTime)
+
+        fitToViewSize = a.getBoolean(
+            R.styleable.UpDownArrow_upa_fitToViewSize,
+            false)
+
+        a.recycle()
+
+        // Call setBackground(Drawable) to set
+        // the arrow drawable as a background.
+        background = super.getBackground()
+    }
+
+    override fun onLayout(changed: Boolean, left: Int,
+                          top: Int, right: Int, bottom: Int) {
+
+        super.onLayout(changed, left, top, right, bottom)
+        if (fitToViewSize) arrow.apply {
+            arrowWidth = this@UpDownArrow.width
+            arrowHeight = this@UpDownArrow.height
+        }
+    }
+
+    fun setProgress(progress: Float) {
+        arrow.progress = progress
+    }
+
+    override fun setBackground(background: Drawable?) {
+        when {
+            // Maybe, the super class will call this method in its constructor,
+            // in that case, 'arrow' is not initialized yet.
+            arrow == null -> super.setBackground(background)
+            background == null -> super.setBackground(arrow)
+            else -> super.setBackground(LayerDrawable(arrayOf(background, arrow)))
+        }
+    }
+
+    override fun getBackground(): Drawable? {
+        val bg = super.getBackground()
+        return if (bg is LayerDrawable) bg.getDrawable(0)
+        else bg
+    }
+}
