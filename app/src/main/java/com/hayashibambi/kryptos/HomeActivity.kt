bd7895a50b81a3c4c2add26fcb9a401cccc0497d
hayashibambi <hayashibambi@gmail.com>
2018-12-13 20:32:47 +0900

    backup

--- app/src/main/java/com/hayashibambi/kryptos/HomeActivity.kt
+++ app/src/main/java/com/hayashibambi/kryptos/HomeActivity.kt
@@ -2,11 +2,23 @@ package com.hayashibambi.kryptos
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : AppCompatActivity() {
+
+    private var sheetBehavior: BottomSheetBehavior<*>? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+        buildSheetBehavior()
+    }
+
+    private fun buildSheetBehavior() {
+        sheetBehavior = BottomSheetBehavior.from(fragment_container)
+        root_layout.post {
+            sheetBehavior?.peekHeight = fragment_container.height - menu.height
+        }
     }
 }
